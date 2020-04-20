4e38e664ca39ac7d06c429080c20788714a93842
William Mittard <billymittard@ymail.com>
2020-04-20 21:10:30 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      return true 
+    else
+      return false
+    end 
+  end 
+
     
-  end
-
+  
   def question?
-
-  end
+    if self.end_with?("?")
+      return true 
+    else 
+      return false 
+    end 
+  end 
 
   def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
+    if self.end_with?("!")
+      return true 
+    else 
+      return false 
+    end 
+  end 
+   
+    def count_sentences
+      new_array = self.split(/[.!?]/).reject {|string| string.empty?}
+      return new_array.length
 end
+end
+    
