1368dba6dc83a07db88bb74daa0609c39e834028
William Mittard <billymittard@ymail.com>
2020-04-22 01:00:38 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,36 +1,23 @@
+
 require 'pry'
 
 class String
 
   def sentence?
-    if self.end_with?(".")
-      return true 
-    else
-      return false
-    end 
-  end 
+    self.end_with?(".")
+  end
 
-    
-  
   def question?
-    if self.end_with?("?")
-      return true 
-    else 
-      return false 
-    end 
-  end 
+    self.end_with?("?")
+  end
 
   def exclamation?
-    if self.end_with?("!")
-      return true 
-    else 
-      return false 
-    end 
-  end 
-   
-    def count_sentences
-      new_array = self.split(/[.!?]/).reject {|string| string.empty?}
-      return new_array.length
-end
+    self.end_with?("!")
+  end
+
+  def count_sentences
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 1}.size
+  end
 end
     
+
